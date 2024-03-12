21083b822f36601347f5ecbb843958f208f1dcc1
Josh <joshuagerome.roque@gmail.com>
2024-03-13 05:10:45 +0800

    Added Reg Form

---
+++ regform/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration Form</title>
+</head>
+
+<style>
+
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: Arial, sans-serif;
+    background-color: #0F9ADA;
+    color: #333;
+    line-height: 1.6;
+    padding: 20px;
+  }
+
+  .container {
+    max-width: 700px;
+    margin: 0 auto;
+  }
+
+  header {
+    text-align: center;
+    margin-bottom: 40px;
+  }
+
+  header img {
+    margin-top: 20px;
+    width: 200px;
+    height: 200px;
+    margin-bottom: 20px;
+  }
+
+  header h1 {
+    font-size: 40px;
+    margin-bottom: 10px;
+    color: #333;
+  }
+
+  header p {
+    font-size: 18px;
+    color: #000;
+    margin-bottom: 10px;
+  }
+
+  header p:last-child {
+    margin-bottom: 20px;
+  }
+
+  main {
+    padding: 40px;
+    background-color: #F7F5EB;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+  }
+
+  section h2 {
+    font-size: 24px;
+    color: #000;
+  }
+
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+
+  ul li {
+    margin-bottom: 10px;
+    color: #333;
+  }
+
+  footer {
+    text-align: center;
+    margin-top: 50px;
+    padding-top: 20px;
+  }
+
+  @media (max-width: 600px) {
+    header h1 {
+      font-size: 28px;
+    }
+
+    header p {
+      font-size: 16px;
+    }
+
+    section h2 {
+      font-size: 20px;
+    }
+
+    header img {
+      width: 100px;
+      height: 100px;
+    }
+  }
+
+  .button {
+    border: none;
+    border-radius: 5px;
+	color: white;
+	padding: 15px 40px;
+	margin: 20px 235px;
+	cursor: pointer;
+	background-color: #035392;
+    font-size: 15px;
+  }
+
+  .button:hover {
+    background-color: #00B3FF;
+  }
+
+  .input {
+    width: 280px;
+    padding: 10px;
+    margin: 10px 0px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 15px;
+    margin-right: 25px;
+    margin-bottom: 30px;
+  }
+
+.row {
+    display: flex;
+    justify-content: space-between;
+    width: 470px;
+}
+
+.row2 {
+    display: flex;
+    justify-content: space-between;
+    width: 480px;
+}
+
+</style>
+
+<body>
+
+<div class="container">
+        <header>
+            <img src="<?php echo "rc.png"; ?>" alt="logo">
+            <h1>Registration Form</h1>
+            <p>Fill out the form carefully for registration</p>
+        </header>
+
+        <main>
+            <section>
+                
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+                    <h2>Student Name</h2>
+                    <input class="input" type="text" name="fname" placeholder="First Name" required>
+                    <input class="input" type="text" name="lname" placeholder="Last Name" required>
+
+                    <div class="row">
+                        <h2>Gender</h2>
+                        <h2>Student Email</h2>
+                    </div>
+
+                    <select class="input" name="gender" required>
+                        <option value="">Please select</option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                    </select>
+                    <input class="input" type="text" id="email" name="email" placeholder="ex: juandelacruz@gmail.com" required>
+
+                    <div class="row2">
+                        <h2>Student ID</h2>
+                        <h2>List of Classes</h2>
+                    </div>
+
+                    <input class="input" type="text" name="id" placeholder="" required>
+                    <select class="input" name="classes" required>
+                        <option value="">Please select</option>
+                        <option value="Math">Math</option>
+                        <option value="Science">Science</option>
+                    </select>
+
+                    <button class="button" type="submit">Submit</button>
+
+                </form>
+
+                <?php
+                    if($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $fname = $_POST['fname'];
+                        $lname = $_POST['lname'];
+                        $gender = $_POST['gender'];
+                        $email = $_POST['email'];
+                        $id = $_POST['id'];
+                        $classes = $_POST['classes'];
+
+                        echo "First Name: $fname<br>";
+                        echo "Last Name: $lname<br>";
+                        echo "Gender: $gender<br>";
+                        echo "Student Email: $email<br>";
+                        echo "Student ID: $id<br>";
+                        echo "List of Classes: $classes<br>";
+
+                    }
+                ?> 
+            </section>
+        </main>
+
+        <footer>
+            <p>&copy; 2024 Joshua Roque</p>
+        </footer>
+    </div>
+    
+</body>
+</html>
